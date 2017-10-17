2ae88885cc114d9e1c490baebd1820050196bd2e
neumannd <daniel.neumann@io-warnemuende.de>
2017-10-17 11:21:38 +0200

    updated formatting of script output

--- bash/01_makeOCEAN_basic/make_ocean_quick.sh
+++ bash/01_makeOCEAN_basic/make_ocean_quick.sh
@@ -5,7 +5,7 @@
 #
 # @author: Daniel Neumann, daniel.neumann@hzg.de
 # @date: 2017-10-17
-# @version: 1.3
+# @version: 1.31
 #
 # Preconditions / Needed Data
 #  GRIDCRO2D file $METEO_IN_NAME in $METEO_DIR
@@ -22,7 +22,7 @@
 ## change into the appropriate directory
 # cd .
 # cd /media/neumannd/work_dell/11_Bewerbung/Arbeitsproben/2017/Bash/01_makeOCEAN_basic
-echo 'Current dir:'
+echo -e '\n  Current dir:'
 echo `pwd`
 
 ## create calculated script file for a call of 'ncap2':
@@ -52,32 +52,34 @@ OUT_DIR="./OCEAN"
 ## check directories
 if [ -e ${TMP_DIR} ]; then
   if [ ! -d ${TMP_DIR} ]; then
-    echo "Temp directory ${TMP_DIR} exists but is not a directory. STOP"
+    echo -e "\n  Temp directory ${TMP_DIR} exists but is not a directory. STOP"
     exit 1
   fi
 else
-  echo "Temp directory ${TMP_DIR} does not exist. Creating"
+  echo -e "\n  Temp directory ${TMP_DIR} does not exist. Creating"
   mkdir ${TMP_DIR}
 fi
 
 if [ -e ${OUT_DIR} ]; then
   if [ ! -d ${OUT_DIR} ]; then
-    echo "Output directory ${OUT_DIR} exists but is not a directory. STOP"
+    echo -e "\n  Output directory ${OUT_DIR} exists but is not a directory. STOP"
     exit 1
   fi
 else
-  echo "Output directory ${OUT_DIR} does not exist. Creating"
+  echo -e "\n  Output directory ${OUT_DIR} does not exist. Creating"
   mkdir ${OUT_DIR}
 fi
 
 if [ ! -e ${METEO_DIR} ]; then
-  echo "Data directory ${METEO_DIR} missing. STOP"
+  echo -e "\n  Data directory ${METEO_DIR} missing. STOP"
   exit 1
   if [ ! -f ${METEO_IN_NAME} ]; then
-    echo "Input data ${METEO_IN_NAME} missing. STOP"
+    echo -e "\n  Input data ${METEO_IN_NAME} missing. STOP"
     exit 1
   fi
 fi
+
+echo ""
 
 
 ## Copy the GRIDCRO2D file and make TSTEP to the record dimension
@@ -86,13 +88,13 @@ ncks -O --mk_rec_dmn TSTEP ${METEO_DIR}/${METEO_IN_NAME} ${TMP_DIR}/cn06.nc
 ## Calculate the OPEN varianle from the LUFRAC_16 and LWMASK variables and
 ## write everything into a tmp file.
 # OPEN = LUFRAC_16 * ((-1) * (LWMASK - 1))
-cdo chname,LUFRAC_16,OPEN -mul -selname,LUFRAC_16 ${TMP_DIR}/cn06.nc -mulc,-1 -subc,1 -selname,LWMASK ${TMP_DIR}/cn06.nc ${TMP_DIR}/tmp01.nc
+cdo -s chname,LUFRAC_16,OPEN -mul -selname,LUFRAC_16 ${TMP_DIR}/cn06.nc -mulc,-1 -subc,1 -selname,LWMASK ${TMP_DIR}/cn06.nc ${TMP_DIR}/tmp01.nc
 
 ## Remove some water grind cells from the new OPEN variable
 # Otherwise, large lakes and river deltas who we open ocean regions.
 case ${GRID_NAME} in
     "CN06")
-       cdo setcindexbox,0,1,90,55,100 -setcindexbox,0,1,68,0,100 -setcindexbox,0,90,100,1,10 -setcindexbox,0,110,115,10,24 -setcindexbox,0,125,140,84,100 ${TMP_DIR}/tmp01.nc ${TMP_DIR}/tmp02.nc
+       cdo -s setcindexbox,0,1,90,55,100 -setcindexbox,0,1,68,0,100 -setcindexbox,0,90,100,1,10 -setcindexbox,0,110,115,10,24 -setcindexbox,0,125,140,84,100 ${TMP_DIR}/tmp01.nc ${TMP_DIR}/tmp02.nc
        ;;
     *)
        cdo -O -s copy ${TMP_DIR}/tmp01.nc ${TMP_DIR}/tmp02.nc
@@ -106,7 +108,7 @@ esac
 # MASK(i,j) = IF( OPEN(i,j) == 0 )      0.0 * OPEN(i,j)
 #             ELSE IF( OPEN(i,j) == 1 ) 2.0 + 0.0 * OPEN(i,j)
 #             ELSE                      1.0 + 0.0 * OPEN(i,j)
-cdo -O merge -chname,OPEN,SURF -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -selname,OPEN ${TMP_DIR}/tmp02.nc -chname,OPEN,MASK -ifthenelse -eqc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -ifthenelse -eqc,1 -selname,OPEN ${TMP_DIR}/tmp02.nc -addc,2 -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -addc,1 -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc ${TMP_DIR}/tmp03.nc
+cdo -s -O merge -chname,OPEN,SURF -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -selname,OPEN ${TMP_DIR}/tmp02.nc -chname,OPEN,MASK -ifthenelse -eqc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -ifthenelse -eqc,1 -selname,OPEN ${TMP_DIR}/tmp02.nc -addc,2 -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc -addc,1 -mulc,0 -selname,OPEN ${TMP_DIR}/tmp02.nc ${TMP_DIR}/tmp03.nc
 
 ## Copy the TFLAG variable from the modified GRIDCRO2D file into an extra
 ## tmp files and duplicate that file.
